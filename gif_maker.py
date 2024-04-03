96972e98375ed0dce1c17480e946844d07ec1d00
VediKa <vedikabhasin@gmail.com>
2024-04-02 18:26:35 -0700

    gif_maker

---
+++ gif_maker.py
@@ -0,0 +1,9 @@
+import imageio.v3 as iio
+
+filenames = ['1.png', '2.png']
+images = [ ]
+
+for filename in filenames:
+  images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
