5ea33109d8a56bc19c0469ba4eb1d936d012c395
VediKa <vedikabhasin@gmail.com>
2024-04-03 21:39:23 -0700

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,10 +1,26 @@
+from PIL import Image
 import qrcode
 
 website_link = 'https://www.vedikabhasin.com/'
 
-qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr = qrcode.QRCode(version = 1, box_size = 7, border = 2)
 qr.add_data(website_link)
 qr.make()
 
-img = qr.make_image(fill_color = 'black', back_color = 'white')
-img.save('website_qr.png')
+
+img = qr.make_image(fill_color = 'grey', back_color = 'transparent')
+
+#transparent bg
+background = Image.new('RGBA', img.size, (255, 255, 255, 0)) 
+background.paste(img)
+
+background.save('website_qr_tp.png')
+background.show()
+
+
+
+# img.save('website_qr.png')
+# img = qr.make_image(fill_color = 'white', back_color = 'black')
+# img.show()
+
+# /Users/vedikabhasin/desktop/fun projects/30-nite-code/
